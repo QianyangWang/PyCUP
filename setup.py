6a27d091eb6a478b82a8c1b6f0be42e556a61cbe
Qianyang Wang <116932670+QianyangWang@users.noreply.github.com>
2026-08-12 15:05:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
   },
   packages=find_packages(exclude=["test", "test.*"]),
   package_data={'pycup':['document/*.pdf']},
-  install_requires=['numpy', 'matplotlib','scipy','pyDOE','statsmodels','pandas','prettytable'],
+  install_requires=['numpy', 'matplotlib','scipy','pydoe','statsmodels','pandas','prettytable'],
   python_requires='>=3'
   )
